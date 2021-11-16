1924e52e73c6cd397e2549bdde2bfda4521de2be
loicboset <loic.boset@gmail.com>
2021-11-16 06:15:34 +0100

    Fix tailwind colour config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      purple: '#8B7FB6',
+    extend: {
+      colors: {
+        purple: '#8B7FB6',
+      },
     },
   },
   variants: {
