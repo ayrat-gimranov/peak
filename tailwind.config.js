d6177878c9d005e08afdf36a3e8fee6491dffe76
loicboset <loic.boset@gmail.com>
2021-11-16 05:50:04 +0100

    Add purple colour to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      purple: '#8B7FB6',
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
